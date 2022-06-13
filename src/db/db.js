4fe6c9ca81f01667712aba45c574d7596376956b
Edgar Mancia <edgar.mancia.919@my.csun.edu>
2022-06-13 15:41:23 -0700

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,23 @@
 import { MongoClient } from 'mongodb';
 
-const DB_NAME = '<db name here>';
+const DB_NAME = 'grocery-manager';
 
 export const db = {
     _dbClient: null,
     connect: async function(url) {
         const client = await MongoClient.connect(url, {
-            poolSize: 10,
+            maxPoolSize: 10,
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-
         this._dbClient = client;
-    },
+    }, 
     getConnection: function() {
         if (!this._dbClient) {
-            console.log('You need to call .connect() first!');
+            console.log('You need to call the connect() function first!');
             process.exit(1);
         }
 
         return this._dbClient.db(DB_NAME);
-    }
+    },
 }
